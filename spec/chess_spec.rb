612af5abef5fbb86aaef51489bc58e3e473a9b3b
Joel Paul <joel.v.paul@outlook.com>
2025-11-07 16:44:41 +1100

    Add test for moving pawn

--- spec/chess_spec.rb
+++ spec/chess_spec.rb
@@ -3,8 +3,33 @@ require_relative '../lib/chess'
 describe Chess do
   subject(:new_game) { described_class.new }
 
+  let(:starting_board) do
+    "  a b c d e f g h\n" + \
+    "8 ♜ ♞ ♝ ♛ ♚ ♝ ♞ ♜ 8\n" + \
+    "7 ♟ ♟ ♟ ♟ ♟ ♟ ♟ ♟ 7\n" + \
+    "6 . . . . . . . . 6\n" + \
+    "5 . . . . . . . . 5\n" + \
+    "4 . . . . . . . . 4\n" + \
+    "3 . . . . . . . . 3\n" + \
+    "2 ♙ ♙ ♙ ♙ ♙ ♙ ♙ ♙ 2\n" + \
+    "1 ♖ ♘ ♗ ♕ ♔ ♗ ♘ ♖ 1\n" + \
+    "  a b c d e f g h"
+  end
+
+  let(:intro_white) { "White's turn. Enter your move (e.g., e2 e4) or a square to show moves (e.g., e2):" }
+  let(:intro_black) { "Black's turn. Enter your move (e.g., e2 e4) or a square to show moves (e.g., e2):" }
+
+  let(:quit) { 'quit' }
+
   describe '#display_board' do
     it 'displays the board in a new game state' do
+      expected = starting_board
+      expect(new_game).to receive(:puts).with(expected)
+      new_game.setup_board
+      new_game.display_board
+    end
+
+    it 'displays moved pawn' do
       expected = \
         "  a b c d e f g h\n" + \
         "8 ♜ ♞ ♝ ♛ ♚ ♝ ♞ ♜ 8\n" + \
@@ -12,12 +37,17 @@ describe Chess do
         "6 . . . . . . . . 6\n" + \
         "5 . . . . . . . . 5\n" + \
         "4 . . . . . . . . 4\n" + \
-        "3 . . . . . . . . 3\n" + \
-        "2 ♙ ♙ ♙ ♙ ♙ ♙ ♙ ♙ 2\n" + \
+        "3(♙). . . . . . . 3\n" + \
+        "2(.)♙ ♙ ♙ ♙ ♙ ♙ ♙ 2\n" + \
         "1 ♖ ♘ ♗ ♕ ♔ ♗ ♘ ♖ 1\n" + \
         "  a b c d e f g h"
+      move = 'a2 a3'
+
+      allow(new_game).to receive(:gets).and_return(move, quit)
       expect(new_game).to receive(:puts).with(expected)
+
       new_game.setup_board
+      new_game.play_turn
       new_game.display_board
     end
   end
